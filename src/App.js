7a3ed26d36bc37beb8331080be56d6d4c3677fec
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-12 00:15:32 +0200

    add Verification Email

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
 import Home from './pages/home';
 import Register from './pages/register';
+import VerificationEmail from './pages/verificationEmail';
 import NotVerified from './pages/notVerified';
 import NotFound from './pages/notFound';
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
           <Route path='/login' exact element={<Login />} />
           <Route path='/register' exact element={<Register />} />
           <Route path='/home' exact element={<Home />} />
+          <Route path='/verification' exact element={<VerificationEmail />} />
           <Route path='/notVerified' exact element={<NotVerified />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
---
+++ src/pages/verificationEmail.js
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+
+export default function verificationEmail() {
+    return (
+        <div>Verification</div>
+    )
+}
