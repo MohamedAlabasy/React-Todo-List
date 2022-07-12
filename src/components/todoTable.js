f1a32da91ef2050c93d98cf91732af68360c3604
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-12 23:11:48 +0200

    add axios

--- src/components/todoTable.js
+++ src/components/todoTable.js
@@ -1,21 +1,28 @@
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { BiShowAlt, BiSearchAlt } from "react-icons/bi";
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import { deleteAlert, searchAlert, showTODO } from './alerts'
 
 export default function TodoTable(status) {
+    const [userData, setUserData] = useState({
+        title: 'Node',
+        isWork: false,
+        tools: ['react', 'node', 'html', 'css']
+    });
+    useEffect(()=>{
+        
+    })
     return (
         <section className="vh-100" style={{ backgroundColor: "#eee" }} >
-            < div className="container py-5 h-100" >
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col-md-12 col-xl-12">
-
-                        <div className="card">
+            < div className="container py-5 h-100 " >
+                <div className="row d-flex justify-content-center align-items-center h-100 ">
+                    <div className="col-md-12 col-xl-12 ">
+                        <div className="card border" style={{ borderRadius: 15 }}>
                             <div className="card-header p-3">
                                 <h5 className="mb-0"><i className="fas fa-tasks me-2"></i> {status}</h5>
                             </div>
                             <div className="card-body" data-mdb-perfect-scrollbar="true" style={{ position: "relative", height: "400px" }}>
-
                                 <table className="table mb-0">
                                     <thead>
                                         <tr className="text-center">
@@ -28,33 +35,37 @@ export default function TodoTable(status) {
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr className="fw-normal align-middle text-center">
-                                            <th>
-                                                <span className="ms-2">Alice Mayer</span>
-                                            </th>
+                                        {userData.tools.map(
+                                            (tool, index) => {
+                                                return <tr tr className="fw-normal align-middle text-center">
+                                                    <th>
+                                                        <span className="ms-2">Alice Mayer</span>
+                                                    </th>
 
-                                            <td className="align-middle">
-                                                <span>Call Sam For payments</span>
-                                            </td>
+                                                    <td className="align-middle">
+                                                        <span>Call Sam For payments</span>
+                                                    </td>
 
-                                            <td className="align-middle">
-                                                <h6 className="mb-0"><span className="badge bg-danger">High priority</span></h6>
-                                            </td>
+                                                    <td className="align-middle">
+                                                        <h6 className="mb-0"><span className="badge bg-danger">High priority</span></h6>
+                                                    </td>
 
-                                            <td className="align-middle">
-                                                <span>17-3-1997</span>
-                                            </td>
-                                            <td className="align-middle">
-                                                <span>17-3-1998</span>
-                                            </td>
+                                                    <td className="align-middle">
+                                                        <span>17-3-1997</span>
+                                                    </td>
+                                                    <td className="align-middle">
+                                                        <span>17-3-1998</span>
+                                                    </td>
 
-                                            <td className="align-middle">
-                                                <Link to="#" onClick={() => { showTODO() }} title="show"><BiShowAlt className="text-success me-3" style={{ width: "30px", height: "30px" }} /></Link>
-                                                <Link to="#" title="search" onClick={() => { searchAlert() }}><BiSearchAlt className="text-danger me-3" style={{ width: "30px", height: "30px" }} /></Link>
-                                                <Link to="/edit" title="edit"><AiFillEdit className="text-danger me-3" style={{ width: "30px", height: "30px" }} /></Link>
-                                                <Link to="#" onClick={() => { deleteAlert() }} title="delete"><AiFillDelete className="text-danger me-3" style={{ width: "30px", height: "30px" }} /></Link>
-                                            </td>
-                                        </tr>
+                                                    <td className="align-middle">
+                                                        <Link to="#" onClick={() => { showTODO() }} title="show"><BiShowAlt className="text-success me-3" style={{ width: "30px", height: "30px" }} /></Link>
+                                                        <Link to="#" title="search" onClick={() => { searchAlert() }}><BiSearchAlt className="text-danger me-3" style={{ width: "30px", height: "30px" }} /></Link>
+                                                        <Link to="/edit" title="edit"><AiFillEdit className="text-danger me-3" style={{ width: "30px", height: "30px" }} /></Link>
+                                                        <Link to="#" onClick={() => { deleteAlert() }} title="delete"><AiFillDelete className="text-danger me-3" style={{ width: "30px", height: "30px" }} /></Link>
+                                                    </td>
+                                                </tr>
+                                            })
+                                        }
                                     </tbody>
                                 </table>
 
